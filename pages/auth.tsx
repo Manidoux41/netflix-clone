42063b223d68890e90eee7c86cf6c125e26b5298
Manfred PARBATIA <superflyman90@gmail.com>
2023-04-24 07:09:37 +0200

    create register function in auth.tsx and install axios

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -13,6 +13,10 @@ const Auth = () => {
         setVariant((currentVariant) => (currentVariant === "login" ? "register" : "login"));
     }, []);
 
+    const register = useCallback(() => {
+        
+    }, []);
+
     return ( 
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
