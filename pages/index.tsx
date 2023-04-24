dafbe0702b9f42e980b02f258efe748cd5ad28c5
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-04-24 13:37:47 +0200

    add logout button in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,34 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
+
+  const { data: user } = useCurrentUser();
+
+
   return (
     <>
-      <h1 className="font-bold text-4xl text-red-500">Netflix Clone</h1>
+      <h1 className="font-semibold text-4xl text-red-500">Netflix Clone</h1>
+      <p className="text-white">Logged in as : {user?.email}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
     </>
   )
 }
