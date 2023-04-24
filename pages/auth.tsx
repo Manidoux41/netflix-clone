a009d603d70442d23248b22d7bb812c393171ae6
Manfred PARBATIA <superflyman90@gmail.com>
2023-04-24 08:06:19 +0200

    use Image component from next otherwise img tag

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { signIn } from 'next-auth/react';
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 const Auth = () => {
 
@@ -51,7 +52,7 @@ const Auth = () => {
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <nav className="px-12 py-5">
-                    <img src="/images/logo.png" alt="logo" className="h-12" />
+                    <Image src="/images/logo.png" alt="logo" className="h-12" />
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
