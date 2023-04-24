0cd507621a664c618baffd56d43c8b2f61a1d473
Manfred PARBATIA <superflyman90@gmail.com>
2023-04-24 07:56:12 +0200

    add redirect router to home page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,12 @@
 import Input from "@/components/Input";
 import axios from "axios";
 import { useCallback, useState } from "react";
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
 
 const Auth = () => {
+
+    const router = useRouter();
 
     const [email, setEmail] = useState("");
     const [name, setname] = useState("");
@@ -14,6 +18,20 @@ const Auth = () => {
         setVariant((currentVariant) => (currentVariant === "login" ? "register" : "login"));
     }, []);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: "/",
+            });
+            router.push("/");
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password, router]);
+
     const register = useCallback(async () => {
         try {
             await axios.post("/api/register", {
@@ -21,10 +39,13 @@ const Auth = () => {
                 name,
                 password,
             });
+            login();
         } catch (error) {
             console.log(error);
         }
-    }, [email, name, password]);
+    }, [email, name, password, login]);
+
+    
 
     return ( 
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -61,7 +82,7 @@ const Auth = () => {
                                 value={password}
                             />
                         </div>
-                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === "login" ? "Sign in" : "Sign up"}
                         </button>
                         <p className="text-neutral-500 mt-12">
